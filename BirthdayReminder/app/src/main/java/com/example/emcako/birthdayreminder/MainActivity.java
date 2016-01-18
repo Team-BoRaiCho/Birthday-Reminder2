8dd62d1935208930caca3dd4796e3d28c1621704
emcako <emcako@abv.bg>
2016-01-18 02:41:03 +0200

    My account page design with datePicker

--- BirthdayReminder/app/src/main/java/com/example/emcako/birthdayreminder/MainActivity.java
+++ BirthdayReminder/app/src/main/java/com/example/emcako/birthdayreminder/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.emcako.birthdayreminder;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.provider.MediaStore;
+import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
@@ -10,13 +13,17 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.DatePicker;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.emcako.birthdayreminder.database.DatabaseHelper;
 import com.example.emcako.birthdayreminder.database.Friend;
 import com.example.emcako.birthdayreminder.fragments.FriendsFragment;
 import com.example.emcako.birthdayreminder.fragments.LocationFragemnt;
 import com.example.emcako.birthdayreminder.fragments.MyAccountFragment;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -92,5 +99,37 @@ public class MainActivity extends AppCompatActivity {
         friend.setBirthday("20.05.1986");
         friend.setImagePath("");
         db.addFriend(friend);
+    }
+
+    public void showDatePickerDialogMain(View v) {
+        DialogFragment newFragment = new MainActivity.DatePickerFragment();
+        newFragment.show(getSupportFragmentManager(), "datePicker");
+    }
+
+    public static class DatePickerFragment extends DialogFragment
+            implements DatePickerDialog.OnDateSetListener {
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Use the current date as the default date in the picker
+            final Calendar c = Calendar.getInstance();
+            int year = c.get(Calendar.YEAR);
+            int month = c.get(Calendar.MONTH);
+            int day = c.get(Calendar.DAY_OF_MONTH);
+
+            // Create a new instance of DatePickerDialog and return it
+            return new DatePickerDialog(getActivity(), this, year, month, day);
+        }
+
+        public void onDateSet(DatePicker view, int year, int month, int day) {
+            // Do something with the date chosen by the user
+            month += 1;
+            TextView textView = (TextView) getActivity().findViewById(R.id.et_birthday_main);
+            if (month < 11) {
+                textView.setText(day + "." + "0" + month + "." + year);
+            } else {
+                textView.setText(day + "." + month + "." + year);
+            }
+        }
     }
 }
